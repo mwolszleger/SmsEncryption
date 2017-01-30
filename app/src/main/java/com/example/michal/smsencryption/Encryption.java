7a37138c09640d8f6d69d0138e3834c0aa1e0994
mwolszleger <mwolszleger@interia.pl>
2017-01-30 19:25:05 +0100

    fixed bug with encryption

--- app/src/main/java/com/example/michal/smsencryption/Encryption.java
+++ app/src/main/java/com/example/michal/smsencryption/Encryption.java
@@ -24,13 +24,33 @@ public class Encryption {
             if(message.charAt(i)<beginChar||message.charAt(i)>endChar)
                 encrypted+=message.charAt(i);
             else
-                encrypted+=(char)((length-message.charAt(i)-beginChar+password.charAt(i%len))%length+beginChar);
+                encrypted+=(char)((message.charAt(i)-beginChar+password.charAt(i%len))%length+beginChar);
         }
         return encrypted;
     }
     public static String DecryptMessage(String message,String password)
     {
-      return EncryptMessage(message,password);
+        int len=password.length();
+        String encrypted="";
+        if(len==0)
+        {
+            return message;
+        }
+        for(int i=0;i<message.length();i++)
+        {
+            if(message.charAt(i)<beginChar||message.charAt(i)>endChar)
+                encrypted+=message.charAt(i);
+            else
+            {
+                char res=message.charAt(i);
+                res-=beginChar;
+                res+=(length-password.charAt(i%len)%length);
+                res=(char)(res%length);
+                res+=beginChar;
+                encrypted+=res;
+            }
+        }
+        return encrypted;
     }
 
 }
